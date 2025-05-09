9da15dfae0720e709eb122c211371e2256d25896
Lola <lolamargareta99@gmail.com>
2025-05-09 17:02:54 +0700

    updated models

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,20 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-// $routes->get('/', 'Welcome::index');
-// $routes->get('welcome/index/(:segment)', 'Welcome::index/\$1');
-// $routes->get('welcome/create', 'Welcome::create');
-// $routes->post('welcome/create', 'Welcome::create');
-// $routes->get('welcome/deleteAll', 'Welcome::deleteAll');
-// $routes->get('welcome/create', 'Welcome::createForm');
-// $routes->post('welcome/create', 'Welcome::create');
-
-// // Change these routes to accept string IDs instead of just numbers
-// $routes->get('welcome/index/(:segment)', 'Welcome::index/\$1');
-// $routes->get('welcome/edit/(:segment)', 'Welcome::edit/\$1');
-// $routes->get('welcome/delete/(:segment)', 'Welcome::delete/$1');
-
-
 // Add these specific routes
 $routes->get('/', 'Welcome::index');
 $routes->get('welcome', 'Welcome::index');
